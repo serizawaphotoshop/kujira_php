e1d463943656961cc878a92a58cd44c40aee9403
a-serizawa <serizawa.photoshop@gmail.com>
2022-06-06 14:04:58 +0900

    20220606

--- result2.php
+++ result2.php
@@ -18,6 +18,38 @@
             <h2>入力内容の確認</h2>
             <section class="formdiv">
                 <dl>
+                    <dt>お名前</dt>
+                    <dd>
+                        <?php
+                            $fullname=$_POST["fullname"];
+                            if(empty($fullname)){
+                                echo "氏名を入力してください";
+                            }else{
+                                echo $fullname;
+                            }
+                        ?>
+                    </dd>
+                </dl>
+            </section>            
+            <section class="formdiv">
+                <dl>
+                    <dt>メールアドレス</dt>
+                    <dd>
+                    <?php
+                            $address=$_POST["address"];
+                            if(empty($address)){
+                                echo "メールアドレスを入力してください";
+                            }elseif (!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\?\*\[|\]%'=~^\{\}\/\+!#&\$\._-])*@([a-zA-Z0-9_-])+\.([a-zA-Z0-9\._-]+)+$/", $address)) {
+                                echo "メールアドレスの形式で入力してください";
+                            }else{
+                                echo $address;
+                            }
+                        ?>
+                    </dd>
+                </dl>
+            </section>
+            <section class="formdiv">
+                <dl>
                     <dt>お問い合わせの種類</dt>
                     <dd>
                         <?php
@@ -53,26 +85,81 @@
                     <dt>当カフェをお知りになったきっかけ</dt>
                     <dd>
                         <?php
-                            if(!isset($_POST['how'])){
-                                $_POST['how']="未選択";
-                            }
-                            $how=$_POST['how'];
+
+                        $how = [
+                            "friend2" => $_POST['howFriend'] ?? false, 
+                            "magazine2" => $_POST['howMagazine'] ?? false
+                        ];
+
+
+                            // if(isset($_POST['howFriend']) && isset($_POST['howMagazine'])){
+                            //     $aaa = $_POST['how']="未選択";
+                            // }
+                            // $how=$_POST['how'];
                             
-                            if($how==="friend"){
+                            if($how["friend2"]){
                                 echo "知り合いの紹介で";
                             }
-                            if($how==="magazine"){
+                            if($how["magazine2"]){
                                 echo "雑誌・Webサイトで見て";
                             }
-                            if($how==="未選択"){
+                            if(!($how["friend2"] || $how["magazine2"])){
                                 echo "チェックされていません";
+                            }
+                            var_dump($how);
+                            echo $how["friend2"];
+                            echo $how["magazine2"];
+
+
+
+                            ?>
+                    </dd>
+                </dl>
+            </section>
+            <section class="formdiv">
+                <dl>
+                    <dt>お問い合わせの件名</dt>
+                    <dd>
+                        <?php
+                            if(!isset($_POST['subject'])){
+                                $_POST['subject']="件名が未入力です";
+                            }
+                            $subject=$_POST['subject'];
+                            
+                            if($subject==="件名が未入力です"){
+                                echo "件名が未入力です";
+                            }
+                            else{
+                                echo $subject;
                             }
                             // var_dump($how);
                             ?>
                     </dd>
                 </dl>
             </section>
-
+            <section class="formdiv">
+                <dl>
+                    <dt>お問い合わせの具体的内容</dt>
+                    <dd>
+                        <?php
+                            if(!isset($_POST['message'])){
+                                $_POST['message']="内容が入力されていません";
+                            }
+                            $message=$_POST['message'];
+                            if(mb_strlen($message)>20){
+                                echo "文字数が20文字を超えています";
+                            }
+                            elseif($message==="内容が入力されていません"){
+                                echo "内容が入力されていません";
+                            }
+                            else{
+                                echo $message;
+                            }
+                            // var_dump($how);
+                            ?>
+                    </dd>
+                </dl>
+            </section>
         </main>
         <!-- メインここまで -->
         <!-- フッター -->
